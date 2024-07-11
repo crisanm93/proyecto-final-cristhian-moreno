fef1d57ec40a1e4299496255d34ac45e5ddc599b
w.site <crisanp74azul@gmail.com>
2024-07-11 02:14:55 -0500

    Api cats

--- js/mainpropietarios.js
+++ js/mainpropietarios.js
@@ -20,3 +20,32 @@ targets.forEach(target => {
         t.classList.add('active');
     });
 });
+
+
+
+// API Key y URL
+const apiKey = 'live_3cPqr6bU7z5lJWfOs0IplE43IxwEprmPyYa3WiqoLtmFZah6wgM8niTTR7Ta3hPX'; // Reemplaza con tu API Key
+const apiUrl = 'https://api.thecatapi.com/v1/images/search';
+
+// Obtener el botón y el contenedor
+const btnCat = document.querySelector('#btn-cat');
+const catImgContainer = document.querySelector('#cat-img-container');
+
+// Escuchar cuando se da clic
+btnCat.addEventListener('click', () => {
+    // Llamar a la API
+    fetch(apiUrl, {
+        headers: {
+            'x-api-key': apiKey
+        }
+    })
+    // Convertir la respuesta a JSON
+    .then(response => response.json())
+    .then(data => {
+        const catImgUrl = data[0].url;
+
+        // Agregar la imagen como background al container
+        catImgContainer.style.backgroundImage = `url('${catImgUrl}')`;
+    })
+    .catch(error => console.error('Error:', error));
+});
